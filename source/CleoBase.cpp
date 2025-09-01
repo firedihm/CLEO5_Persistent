737d09d0d98228acf54d20e79ae9988e17af3392
firedihm <42544757+firedihm@users.noreply.github.com>
2025-09-01 14:05:13 +0300

    Update CleoBase.cpp

--- source/CleoBase.cpp
+++ source/CleoBase.cpp
@@ -2,247 +2,228 @@
 #include "CleoBase.h"
 #include "Singleton.h"
 
+using namespace CLEO;
 
-namespace CLEO
+CCleoInstance CleoInstance;
+
+void CCleoInstance::Start(InitStage stage)
 {
-    CCleoInstance CleoInstance;
+        if (stage > InitStage::Done || stage != GetNextInitStage()) 
+                return;
 
-    inline CCleoInstance::~CCleoInstance()
-    {
-        Stop();
-    }
+        if (stage == InitStage::Initial) {
+                TRACE("CLEO initialization: Phase 1");
 
-    void __cdecl CCleoInstance::OnUpdateGameLogics()
-    {
-        CleoInstance.CallCallbacks(eCallbackId::GameProcessBefore); // execute registered callbacks
+                FS::create_directory(Filepath_Cleo);
+                FS::create_directory(Filepath_Cleo + "\\cleo_modules");
+                FS::create_directory(Filepath_Cleo + "\\cleo_plugins");
+                FS::create_directory(Filepath_Cleo + "\\cleo_saves");
 
-        CleoInstance.UpdateGameLogics_Orig(); // call original function
+                OpcodeInfoDb.Load((Filepath_Cleo + "\\.config\\sa.json").c_str());
 
-        CleoInstance.CallCallbacks(eCallbackId::GameProcessAfter); // execute registered callbacks
-    }
+                CodeInjector.OpenReadWriteAccess(); // must do this earlier to ensure plugins write access on init
+                GameMenu.Inject(CodeInjector);
+                DmaFix.Inject(CodeInjector);
+                OpcodeSystem.Inject(CodeInjector);
+                ScriptEngine.Inject(CodeInjector);
 
-    HWND CCleoInstance::OnCreateMainWnd(HINSTANCE hinst)
-    {
+                CodeInjector.ReplaceFunction(OnCreateMainWnd, VersionManager.TranslateMemoryAddress(MA_CALL_CREATE_MAIN_WINDOW), &CreateMainWnd_Orig);
+
+                CodeInjector.ReplaceFunction(OnScmInit1, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM1), &ScmInit1_Orig);
+                CodeInjector.ReplaceFunction(OnScmInit2, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM2), &ScmInit2_Orig);
+                CodeInjector.ReplaceFunction(OnScmInit3, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM3), &ScmInit3_Orig);
+
+                CodeInjector.ReplaceFunction(OnGameShutdown, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_SHUTDOWN), &GameShutdown_Orig);
+
+                CodeInjector.ReplaceFunction(OnGameRestart1, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_1), &GameRestart1_Orig);
+                CodeInjector.ReplaceFunction(OnGameRestart2, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_2), &GameRestart2_Orig);
+                CodeInjector.ReplaceFunction(OnGameRestart3, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_3), &GameRestart3_Orig);
+
+                OpcodeSystem.Init();
+                PluginSystem.LoadPlugins();
+        } else if (stage == InitStage::OnDraw) {
+                TRACE("CLEO initialization: Phase 2"); // delayed until menu background drawing
+
+                const_cast<std::string&>(Filepath_User) = GetUserDirectory(); // force update now, as it could be modifed by PortableGTA.asi
+
+                ScriptEngine.InjectLate(CodeInjector);
+
+                CodeInjector.InjectFunction(GetScriptStringParam, gaddrof(::CRunningScript::ReadTextLabelFromScript));
+                CodeInjector.ReplaceFunction(OnDebugDisplayTextBuffer_Idle, VersionManager.TranslateMemoryAddress(MA_CALL_DEBUG_DISPLAY_TEXT_BUFFER_IDLE), &GameRestartDebugDisplayTextBuffer_IdleOrig);
+                CodeInjector.ReplaceFunction(OnDebugDisplayTextBuffer_Frontend, VersionManager.TranslateMemoryAddress(MA_CALL_DEBUG_DISPLAY_TEXT_BUFFER_FRONTEND), &GameRestartDebugDisplayTextBuffer_FrontendOrig);
+                CodeInjector.ReplaceFunction(OnUpdateGameLogics, VersionManager.TranslateMemoryAddress(MA_CALL_UPDATE_GAME_LOGICS), &UpdateGameLogics_Orig);
+
+                PluginSystem.LogLoadedPlugins();
+        }
+
+        m_InitStage = stage;
+}
+
+void CCleoInstance::Stop()
+{
+        GameEnd();
+        PluginSystem.UnloadPlugins();
+        m_InitStage = InitStage::None;
+}
+
+HWND CCleoInstance::OnCreateMainWnd(HINSTANCE hinst)
+{
         CleoSingletonCheck(); // check once for CLEO.asi duplicates
 
-        auto window = CleoInstance.CreateMainWnd_Orig(hinst); // call original
+        auto window = CreateMainWnd_Orig(hinst);
 
         // redirect window handling procedure
-        *((size_t*)&CleoInstance.MainWndProc_Orig) = GetWindowLongPtr(window, GWLP_WNDPROC); // store original address
+        *((size_t*)&MainWndProc_Orig) = GetWindowLongPtr(window, GWLP_WNDPROC);
         SetWindowLongPtr(window, GWLP_WNDPROC, (LONG)OnMainWndProc);
 
         return window;
-    }
+}
 
-    LRESULT __stdcall CCleoInstance::OnMainWndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam)
-    {
-        switch (msg)
-        {
+LRESULT __stdcall CCleoInstance::OnMainWndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+        switch (msg) {
             case WM_ACTIVATE:
-                CleoInstance.CallCallbacks(eCallbackId::MainWindowFocus, wparam != 0);
+                CallCallbacks(eCallbackId::MainWindowFocus, wparam != 0);
                 break;
-
             case WM_KILLFOCUS:
-                CleoInstance.CallCallbacks(eCallbackId::MainWindowFocus, false);
+                CallCallbacks(eCallbackId::MainWindowFocus, false);
                 break;
         }
 
-        return CleoInstance.MainWndProc_Orig(wnd, msg, wparam, lparam);
-    }
+        return MainWndProc_Orig(wnd, msg, wparam, lparam);
+}
 
-    void CCleoInstance::OnScmInit1()
-    {
-        CleoInstance.ScmInit1_Orig(); // call original
-        CleoInstance.GameBegin();
-    }
+void CCleoInstance::GameBegin()
+{
+        if (m_bGameInProgress)
+                return;
 
-    void CCleoInstance::OnScmInit2() // load save
-    {
-        CleoInstance.ScmInit2_Orig(); // call original
-        CleoInstance.GameBegin();
-    }
-
-    void CCleoInstance::OnScmInit3()
-    {
-        CleoInstance.ScmInit3_Orig(); // call original
-        CleoInstance.GameBegin();
-    }
-
-    void __declspec(naked) CCleoInstance::OnGameShutdown()
-    {
-        CleoInstance.GameEnd();
-        static DWORD oriFunc;
-        oriFunc = (DWORD)(CleoInstance.GameShutdown_Orig);
-        _asm jmp oriFunc
-    }
-
-    void __declspec(naked) CCleoInstance::OnGameRestart1()
-    {
-        CleoInstance.GameEnd();
-        static DWORD oriFunc;
-        oriFunc = (DWORD)(CleoInstance.GameRestart1_Orig);
-        _asm jmp oriFunc
-    }
-
-    void __declspec(naked) CCleoInstance::OnGameRestart2()
-    {
-        CleoInstance.GameEnd();
-        static DWORD oriFunc;
-        oriFunc = (DWORD)(CleoInstance.GameRestart2_Orig);
-        _asm jmp oriFunc
-    }
-
-    void __declspec(naked) CCleoInstance::OnGameRestart3()
-    {
-        CleoInstance.GameEnd();
-        static DWORD oriFunc;
-        oriFunc = (DWORD)(CleoInstance.GameRestart3_Orig);
-        _asm jmp oriFunc
-    }
-
-    void __cdecl CCleoInstance::OnDebugDisplayTextBuffer_Idle()
-    {
-        CleoInstance.GameRestartDebugDisplayTextBuffer_IdleOrig(); // call original
-        CleoInstance.CallCallbacks(eCallbackId::DrawingFinished); // execute registered callbacks
-    }
-
-    void __cdecl CCleoInstance::OnDebugDisplayTextBuffer_Frontend()
-    {
-        CleoInstance.GameRestartDebugDisplayTextBuffer_FrontendOrig(); // call original
-        CleoInstance.CallCallbacks(eCallbackId::DrawingFinished); // execute registered callbacks
-    }
-
-    void CCleoInstance::Start(InitStage stage)
-    {
-        if (stage > InitStage::Done) return; // invalid argument
-
-        auto nextStage = InitStage(m_initStage + 1);
-        if (stage != nextStage) return;
-
-        if (stage == InitStage::Initial)
-        {
-            TRACE("CLEO initialization: Phase 1");
-
-            FS::create_directory(Filepath_Cleo);
-            FS::create_directory(Filepath_Cleo + "\\cleo_modules");
-            FS::create_directory(Filepath_Cleo + "\\cleo_plugins");
-            FS::create_directory(Filepath_Cleo + "\\cleo_saves");
-
-            OpcodeInfoDb.Load((Filepath_Cleo + "\\.config\\sa.json").c_str());
-
-            CodeInjector.OpenReadWriteAccess(); // must do this earlier to ensure plugins write access on init
-            GameMenu.Inject(CodeInjector);
-            DmaFix.Inject(CodeInjector);
-            OpcodeSystem.Inject(CodeInjector);
-            ScriptEngine.Inject(CodeInjector);
-
-            CodeInjector.ReplaceFunction(OnCreateMainWnd, VersionManager.TranslateMemoryAddress(MA_CALL_CREATE_MAIN_WINDOW), &CreateMainWnd_Orig);
-
-            CodeInjector.ReplaceFunction(OnScmInit1, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM1), &ScmInit1_Orig);
-            CodeInjector.ReplaceFunction(OnScmInit2, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM2), &ScmInit2_Orig);
-            CodeInjector.ReplaceFunction(OnScmInit3, VersionManager.TranslateMemoryAddress(MA_CALL_INIT_SCM3), &ScmInit3_Orig);
-
-            CodeInjector.ReplaceFunction(OnGameShutdown, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_SHUTDOWN), &GameShutdown_Orig);
-
-            CodeInjector.ReplaceFunction(OnGameRestart1, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_1), &GameRestart1_Orig);
-            CodeInjector.ReplaceFunction(OnGameRestart2, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_2), &GameRestart2_Orig);
-            CodeInjector.ReplaceFunction(OnGameRestart3, VersionManager.TranslateMemoryAddress(MA_CALL_GAME_RESTART_3), &GameRestart3_Orig);
-
-            OpcodeSystem.Init();
-            PluginSystem.LoadPlugins();
-        }
-        
-        // delayed until menu background drawing
-        if (stage == InitStage::OnDraw)
-        {
-            TRACE("CLEO initialization: Phase 2");
-
-            const_cast<std::string&>(Filepath_User) = GetUserDirectory(); // force update now, as it could be modifed by PortableGTA.asi
-
-            ScriptEngine.InjectLate(CodeInjector);
-
-            CodeInjector.InjectFunction(GetScriptStringParam, gaddrof(::CRunningScript::ReadTextLabelFromScript));
-            CodeInjector.ReplaceFunction(OnDebugDisplayTextBuffer_Idle, VersionManager.TranslateMemoryAddress(MA_CALL_DEBUG_DISPLAY_TEXT_BUFFER_IDLE), &GameRestartDebugDisplayTextBuffer_IdleOrig);
-            CodeInjector.ReplaceFunction(OnDebugDisplayTextBuffer_Frontend, VersionManager.TranslateMemoryAddress(MA_CALL_DEBUG_DISPLAY_TEXT_BUFFER_FRONTEND), &GameRestartDebugDisplayTextBuffer_FrontendOrig);
-            CodeInjector.ReplaceFunction(OnUpdateGameLogics, VersionManager.TranslateMemoryAddress(MA_CALL_UPDATE_GAME_LOGICS), &UpdateGameLogics_Orig);
-
-            PluginSystem.LogLoadedPlugins();
-        }
-
-        m_initStage = stage;
-    }
-
-    void CCleoInstance::Stop()
-    {
-        GameEnd();
-        PluginSystem.UnloadPlugins();
-        m_initStage = InitStage::None;
-    }
-
-    void CCleoInstance::GameBegin()
-    {
-        if (m_bGameInProgress) return;
         m_bGameInProgress = true;
+        m_saveSlot = FrontEndMenuManager.m_bWantToLoad ? FrontEndMenuManager.m_nSelectedSaveGame : -1;
 
-        saveSlot = FrontEndMenuManager.m_bWantToLoad ? FrontEndMenuManager.m_nSelectedSaveGame : -1;
+        TRACE("Starting new game session, save slot: %d", m_saveSlot);
 
-        TRACE("Starting new game session, save slot: %d", saveSlot);
+        Start(CCleoInstance::InitStage::OnDraw); // late initialization if not done yet
+        CallCallbacks(eCallbackId::GameBegin, m_saveSlot);
+}
 
-        // late initialization if not done yet
-        CleoInstance.Start(CCleoInstance::InitStage::OnDraw);
+void CCleoInstance::GameEnd()
+{
+        if (!m_bGameInProgress)
+                return;
 
-        // execute registered callbacks
-        CleoInstance.CallCallbacks(eCallbackId::GameBegin, saveSlot);
-    }
-
-    void CCleoInstance::GameEnd()
-    {
-        if (!m_bGameInProgress) return;
         m_bGameInProgress = false;
+        m_saveSlot = -1;
 
         TRACE("Ending current game session");
-        CleoInstance.CallCallbacks(eCallbackId::GameEnd); // execute registered callbacks
+
+        CallCallbacks(eCallbackId::GameEnd);
         ScriptEngine.GameEnd();
-        OpcodeSystem.FinalizeScriptObjects();
+        OpcodeSystem.FinalizeScriptObjects();        
+}
 
-        saveSlot = -1;
-    }
+void CCleoInstance::GameRestart()
+{
+        if (!m_bGameInProgress)
+                return;
 
-    void CCleoInstance::AddCallback(eCallbackId id, void* func)
-    {
-        m_callbacks[id].insert(func);
-    }
+        m_bGameInProgress = false;
+        m_saveSlot = -1;
 
-    void CCleoInstance::RemoveCallback(eCallbackId id, void* func)
-    {
-        m_callbacks[id].erase(func);
-    }
+        TRACE("Ending current game session");
 
-    const std::set<void*>& CCleoInstance::GetCallbacks(eCallbackId id)
-    {
-        return m_callbacks[id];
-    }
+        CallCallbacks(eCallbackId::GameEnd);
+        ScriptEngine.GameRestart();
+        OpcodeSystem.FinalizeScriptObjects();        
+}
 
-    void CCleoInstance::CallCallbacks(eCallbackId id)
-    {
-        for (void* func : GetCallbacks(id))
-        {
+void CCleoInstance::OnScmInit1()
+{
+        ScmInit1_Orig();
+        GameBegin();
+}
+
+void CCleoInstance::OnScmInit2() // load save
+{
+        ScmInit2_Orig();
+        GameBegin();
+}
+
+void CCleoInstance::OnScmInit3()
+{
+        ScmInit3_Orig();
+        GameBegin();
+}
+
+void __declspec(naked) CCleoInstance::OnGameShutdown()
+{
+        GameEnd();
+        static DWORD oriFunc;
+        oriFunc = (DWORD)(GameShutdown_Orig);
+        _asm jmp oriFunc
+}
+
+void __declspec(naked) CCleoInstance::OnGameRestart1()
+{
+        GameRestart();
+        static DWORD oriFunc;
+        oriFunc = (DWORD)(GameRestart1_Orig);
+        _asm jmp oriFunc
+}
+
+void __declspec(naked) CCleoInstance::OnGameRestart2()
+{
+        GameRestart();
+        static DWORD oriFunc;
+        oriFunc = (DWORD)(GameRestart2_Orig);
+        _asm jmp oriFunc
+}
+
+void __declspec(naked) CCleoInstance::OnGameRestart3()
+{
+        GameRestart();
+        static DWORD oriFunc;
+        oriFunc = (DWORD)(GameRestart3_Orig);
+        _asm jmp oriFunc
+}
+
+void __cdecl CCleoInstance::OnDebugDisplayTextBuffer_Idle()
+{
+        GameRestartDebugDisplayTextBuffer_IdleOrig();
+        CallCallbacks(eCallbackId::DrawingFinished);
+}
+
+void __cdecl CCleoInstance::OnDebugDisplayTextBuffer_Frontend()
+{
+        GameRestartDebugDisplayTextBuffer_FrontendOrig();
+        CallCallbacks(eCallbackId::DrawingFinished);
+}
+
+void __cdecl CCleoInstance::OnUpdateGameLogics()
+{
+        CallCallbacks(eCallbackId::GameProcessBefore);
+        UpdateGameLogics_Orig();
+        CallCallbacks(eCallbackId::GameProcessAfter);
+}
+
+void CCleoInstance::CallCallbacks(eCallbackId id)
+{
+        for (void* func : m_callbacks[id]) {
             typedef void WINAPI callback(void);
             ((callback*)func)();
         }
-    }
+}
 
-    void CCleoInstance::CallCallbacks(eCallbackId id, DWORD arg)
-    {
-        for (void* func : GetCallbacks(id))
-        {
+void CCleoInstance::CallCallbacks(eCallbackId id, DWORD arg)
+{
+        for (void* func : m_callbacks[id]) {
             typedef void WINAPI callback(DWORD);
             ((callback*)func)(arg);
         }
-    }
+}
 
-    void WINAPI CLEO_ResolvePath(CLEO::CRunningScript* thread, char* inOutPath, DWORD pathMaxLen)
-    {
+void WINAPI CLEO_ResolvePath(CLEO::CRunningScript* thread, char* inOutPath, DWORD pathMaxLen)
+{
         if (thread == nullptr || inOutPath == nullptr || pathMaxLen < 2)
         {
             return; // invalid param
@@ -254,10 +235,10 @@ namespace CLEO
             resolved.resize(pathMaxLen - 1); // and terminator character
 
         std::memcpy(inOutPath, resolved.c_str(), resolved.length() + 1); // with terminator
-    }
+}
 
-    void WINAPI CLEO_StringListFree(StringList list)
-    {
+void WINAPI CLEO_StringListFree(StringList list)
+{
         if (list.count > 0 && list.strings != nullptr)
         {
             for (DWORD i = 0; i < list.count; i++)
@@ -267,10 +248,10 @@ namespace CLEO
 
             free(list.strings);
         }
-    }
+}
 
-    StringList WINAPI CLEO_ListDirectory(CLEO::CRunningScript* thread, const char* searchPath, BOOL listDirs, BOOL listFiles)
-    {
+StringList WINAPI CLEO_ListDirectory(CLEO::CRunningScript* thread, const char* searchPath, BOOL listDirs, BOOL listFiles)
+{
         if (searchPath == nullptr)
             return {}; // invalid param
 
@@ -311,6 +292,4 @@ namespace CLEO
         FindClose(hSearch);
 
         return CreateStringList(found);
-    }
 }
-
