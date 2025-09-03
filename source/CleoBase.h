c4ed7d7dd63c646033fcb04ebd4aa4423b705b91
firedihm <42544757+firedihm@users.noreply.github.com>
2025-09-03 13:57:34 +0300

    Update CleoBase.h

--- source/CleoBase.h
+++ source/CleoBase.h
@@ -13,17 +13,17 @@
 namespace CLEO
 {
 
-enum class InitStage
-{
-        None,
-        Initial,
-        OnDraw,
-        Done = OnDraw
-};
-
 class CCleoInstance
 {
     public:
+        enum class InitStage
+        {
+                None,
+                Initial,
+                OnDraw,
+                Done = OnDraw
+        };
+
         // order here defines init and deinit order!
         CDmaFix		            DmaFix;
         CGameMenu		        GameMenu;
@@ -41,14 +41,14 @@ class CCleoInstance
         void Start(InitStage stage);
         void Stop();
 
-        bool IsStarted() const { return m_InitStage != InitStage::None; }
-        int GetSaveSlot() const { return m_saveSlot; }
-
-        const std::set<void*>& GetCallbacks(eCallbackId id) { return m_callbacks[id]; }
         void AddCallback(eCallbackId id, void* func) { m_callbacks[id].insert(func); }
         void RemoveCallback(eCallbackId id, void* func) { m_callbacks[id].erase(func); }
         void CallCallbacks(eCallbackId id);
         void CallCallbacks(eCallbackId id, DWORD arg);
+
+        bool IsStarted() const { return m_InitStage != InitStage::None; }
+        int GetSaveSlot() const { return m_saveSlot; }
+        const std::set<void*>& GetCallbacks(eCallbackId id) { return m_callbacks[id]; }
 
     private:
         InitStage m_InitStage = InitStage::None;
@@ -60,37 +60,33 @@ class CCleoInstance
         void GameEnd();
         void GameRestart();
 
-        // call for InitInstance
+        // hooks
         HWND(__cdecl* CreateMainWnd_Orig)(HINSTANCE) = nullptr;
         LRESULT(__stdcall* MainWndProc_Orig)(HWND, UINT, WPARAM, LPARAM) = nullptr;
         static HWND __cdecl OnCreateMainWnd(HINSTANCE hinst);
         static LRESULT __stdcall OnMainWndProc(HWND, UINT, WPARAM, LPARAM);
 
-        // calls to CTheScripts::Init
         void(__cdecl* ScmInit1_Orig)() = nullptr;
         void(__cdecl* ScmInit2_Orig)() = nullptr;
         void(__cdecl* ScmInit3_Orig)() = nullptr;
-        static void OnScmInit1();
-        static void OnScmInit2();
-        static void OnScmInit3();
+        static void __cdecl OnScmInit1();
+        static void __cdecl OnScmInit2();
+        static void __cdecl OnScmInit3();
 
-        // call for Game::Shutdown
         void(__cdecl* GameShutdown_Orig)() = nullptr;
-        static void OnGameShutdown();
+        static void __cdecl OnGameShutdown();
 
-        // calls for Game::ShutDownForRestart
         void(__cdecl* GameRestart1_Orig)() = nullptr;
         void(__cdecl* GameRestart2_Orig)() = nullptr;
         void(__cdecl* GameRestart3_Orig)() = nullptr;
-        static void OnGameRestart1();
-        static void OnGameRestart2();
-        static void OnGameRestart3();
+        static void __cdecl OnGameRestart1();
+        static void __cdecl OnGameRestart2();
+        static void __cdecl OnGameRestart3();
 
-        // calls to CDebug::DebugDisplayTextBuffer
         void(__cdecl* GameRestartDebugDisplayTextBuffer_Idle_Orig)() = nullptr;
         void(__cdecl* GameRestartDebugDisplayTextBuffer_Frontend_Orig)() = nullptr;
-        static void OnDebugDisplayTextBuffer_Idle();
-        static void OnDebugDisplayTextBuffer_Frontend();
+        static void __cdecl OnDebugDisplayTextBuffer_Idle();
+        static void __cdecl OnDebugDisplayTextBuffer_Frontend();
 
         void(__cdecl* UpdateGameLogics_Orig)() = nullptr;
         static void __cdecl OnUpdateGameLogics();
